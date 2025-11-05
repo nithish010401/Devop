7d821edb362d000ff190514f95060f6fcc14a856
vabanagiriravi555@gmail.com <vabanagiriravi555@gmail.com>
2025-11-05 19:08:30 +0530

    intial

---
+++ swap.java
@@ -0,0 +1,14 @@
+ class swap {
+    public static void main(String[] args) {
+        int a=10,b=20;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+        
+    }
+}
